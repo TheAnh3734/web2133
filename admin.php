00500d9771ec3a96568b1480cea6a8ef5770ae55
TheAnh3734 <55874840+TheAnh3734@users.noreply.github.com>
2019-11-03 22:19:38 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -69,13 +69,13 @@
 	<div class="header">
 		<div class="nava">
 			<ul>
-				<li><a href="https://designweb.herokuapp.com/ATN.php">Trở về giao diện khách hàng</a></li>
+				<li><a href="https://newapppppppppppppp.herokuapp.com">Trở về giao diện khách hàng</a></li>
 			</ul>
 			</div>
 		<div class="banner">
 		 		<div class="Home">
 		 			<p>Giao diện dành riêng cho admin</p>
-					<a href="https://designweb.herokuapp.com/ATN.php">ATN Shop</a>
+					<a href="https://newapppppppppppppp.herokuapp.com">ATN Shop</a>
 				</div>
 				<div class="Search">
 					<div class="Search1">
@@ -208,23 +208,7 @@
 
 
 	</div>
-	<div class="footer">
-		<table  cellspacing="0" cellpadding="10" width= 100% align="center" >
-			<tr >
-			<th style="font-size: 17px; color:#FFFFFF"  >ATN_KÊNH MUA SẮM & DỊCH VỤ TRỰC TUYẾN HÀNG ĐẦU VIỆT NAM!</th>
-			<th  rowspan="2" style=" color:#FFFFFF" > CÔNG TY TNHH ATN <br>
- 													Giấy CNĐKDN: 289037490 – Ngày cấp: 06/5/2005, được sửa đổi lần thứ 17 ngày 24/7/2017. <br>
- 													Cơ quan cấp: Phòng Đăng ký kinh doanh – Sở kế hoạch và Đầu tư hà Nội. <br>
- 													Địa chỉ đăng ký kinh doanh: Tầng 71, Tòa Nhà Keangnam, E6, Phạm Hùng, Phường Mễ Trì, Quận Nam Từ Liêm, Hà Nội, Việt Nam <br>  <br><br>    @ATN 2019
- 			</th>
-			</tr>
-			<tr >
-				<td ><div align="center" style="padding-top:0%, width= 20px; color:#FFFFFF" >	
-				</style>Mua hàng trực tuyến (mua hàng online) mang lại sự tiện lợi, lựa chọn đa dạng hơn và các dịch vụ tốt hơn cho người tiêu dùng, thế nhưng người tiêu dùng Việt Nam vẫn chưa tận hưởng được những tiện ích đó. Chính vì vậy ATN Việt Nam được triển khai với mong muốn trở thành trung tâm mua sắm trực tuyến số 1 tại Việt Nam, nơi bạn có thể chọn lựa mọi thứ để chăm sóc thứ đồ chơi bạn yêu thích.... Chúng tôi có tất cả!</div></td>	
-			</tr>
-		</table>
-	</div>
-
+	
 
 </body>
 </html>
